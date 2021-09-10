c4247b290300086b865e72fb56f7e09b6234b978
Mike Onishchuk <mike@mike.od.ua>
2021-09-10 11:38:47 +0300

    Lesson-002, DONE

--- Lesson-001/src/HelloWorld.java
+++ Lesson-001/src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello worldm from Mike!");
+        System.out.println("Hello world from Mike!");
     }
 }
