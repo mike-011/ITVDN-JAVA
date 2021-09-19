b27fd6b8b831d7c9c70fc9e7d52482a2e108f17c
Mike Onishchuk <mike@mike.od.ua>
2021-09-19 20:20:06 +0300

    DONE: LEsson 3, task 3 CIRCLE

---
+++ Lesson-003/src/Circle.java
@@ -0,0 +1,14 @@
+public class Circle {
+    public static void main(String[] args) {
+        /* Задание 3
+        DONE: Используя IntelliJ IDEA, создайте класс Circle.
+        DONE: Создайте константу с именем PI (число π «пи»), создайте переменную радиус с именем – r.
+        DONE: Используя формулу πR2, вычислите площадь круга и выведите результат на экран
+         */
+        final double PI = 3.1415;
+        double r = 2.5, S;
+
+        S = Math.pow(PI*r,2);
+        System.out.println("Площадь круга с радиусом " + r + "м. = "+ S +" метров");
+    }
+}
